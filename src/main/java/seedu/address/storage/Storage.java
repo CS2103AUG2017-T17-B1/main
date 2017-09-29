cfddc49a3ddb5429542fac50c1a731b7c28f44f9
khooroko <khoowp25@hotmail.com>
2017-09-30 02:26:22 +0800

    StorageManagerTest.java: add non-null assertion test for getBackupAddressBookFilePath()

--- src/main/java/seedu/address/storage/Storage.java
+++ src/main/java/seedu/address/storage/Storage.java
@@ -24,6 +24,9 @@ public interface Storage extends AddressBookStorage, UserPrefsStorage {
     String getAddressBookFilePath();
 
     @Override
+    String getBackupAddressBookFilePath();
+
+    @Override
     Optional<ReadOnlyAddressBook> readAddressBook() throws DataConversionException, IOException;
 
     @Override
